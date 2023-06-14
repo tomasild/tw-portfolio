eb9cfa226359f83c2c06e3a3a77a64f592d2f61d
tomasild <tomas.ild.93@gmail.com>
2023-06-13 23:25:35 -0400

    twconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,33 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        primary: "#000000",
+      backgroundImage: {
+        'lofi': "url('../dist/img/lofi-lg.png')",
+        'front': "url('../dist/img/front.jpg')",
+        'lofi': "url('../dist/img/lofi-lg.png')",
+        'lofi': "url('../dist/img/lofi-lg.png')",
+        'lofi': "url('../dist/img/lofi-lg.png')",
+        'lofi': "url('../dist/img/lofi-lg.png')",
+        'lofi': "url('../dist/img/lofi-lg.png')",
+        'lofi': "url('../dist/img/lofi-lg.png')",
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'), 
+        "primary": '#CC2D4A',
+        "secondary": '#8FA206',
+        'terciary': '#61AEC9'
+      }),
+      textColor: {
+        "primary": '#CC2D4A',
+        "secondary": '#8FA206',
+        'terciary': '#61AEC9'
+      },
+      fontFamily: {
+        Montserrat: [
+          'Montserrat', 'sans-serif'
+        ]
       }
-    },
+    }
   },
   plugins: [],
 }
-
